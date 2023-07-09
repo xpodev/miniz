6b4eb8e188c43d690db4e4b1c5494580d510d324
Binyamin Y Cohen <binyamincohen555@gmail.com>
2023-07-09 21:49:48 +0300

    Add ScopeProtocol

--- miniz/interfaces/base.py
+++ miniz/interfaces/base.py
@@ -7,3 +7,16 @@ class IMiniZObject(ObjectProtocol):
 
 class INamed(IMiniZObject):
     name: str | None
+
+
+class ScopeProtocol:
+    """
+    Represents a scope
+    """
+
+    def get_name(self, name: str) -> ObjectProtocol:
+        """
+        Returns the value associated with the given `name` in this scope.
+
+        :raises NameNotFoundError: if the given name could not be found.
+        """
